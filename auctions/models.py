4908cc5167c84ee6cd8a661f082cf40244acfcd6
Ayrton Machado <ayrtonsoaresm@gmail.com>
2024-02-18 17:25:36 -0300

    add Create Listing in views and models, add a active list and css in

--- auctions/models.py
+++ auctions/models.py
@@ -7,9 +7,10 @@ class AuctionListing(models.Model):
     description = models.TextField(max_length=200)
     bidstart = models.IntegerField()
     urlImage = models.CharField(max_length=300)
+    category = models.CharField(max_length=64)
 
     def __str__(self):
-        return f'{self.title}\n{self.description}\n{self.bidstart}'
+        return f'ID: {self.id} Título: {self.title}\nDescrição: {self.description}\nBID: {self.bidstart}\n CATG: {self.category}\n URL: {self.urlImage}\n'
 
 class Bids(models.Model):
     pass
--- auctions/views.py
+++ auctions/views.py
@@ -3,12 +3,24 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
+from .models import User, AuctionListing
 
-from .models import User
-
+class Auction_Listing(forms.ModelForm):
+    class Meta:
+        model = AuctionListing
+        fields = ['title', 'description', 'bidstart', 'urlImage', 'category']
+        
+    def __init__(self, *args, **kwargs):
+        super(Auction_Listing, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.required = False
 
 def index(request):
-    return render(request, "auctions/index.html")
+    auctions = AuctionListing.objects.all()
+    return render(request, "auctions/index.html", {
+        'auctions': auctions
+    })
 
 
 def login_view(request):
@@ -63,4 +75,12 @@ def register(request):
         return render(request, "auctions/register.html")
 
 def create_listing(request):
-    return render(request, 'auctions/createlisting.html')
+    auction = Auction_Listing(request.POST)
+    if request.method == 'POST':
+        auction = Auction_Listing(request.POST)
+        if auction.is_valid():
+            auction.save()
+            return HttpResponseRedirect(reverse('index'))
+    return render(request, 'auctions/createlisting.html', {
+        'auction': auction
+    })
